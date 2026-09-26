1d437e50a2235596623db5f168fd239a82df2149
PasoStudio73 <paso.studio73@gmail.com>
2026-09-26 15:54:09 +0200

    starting Frames refactor

--- test/frames.jl
+++ test/frames.jl
@@ -1,7 +1,7 @@
 using Test
 using Audio911
 
-test_files_dir()    = joinpath(dirname(@__FILE__), "test_files")
+test_files_dir() = joinpath(dirname(@__FILE__), "test_files")
 test_file(filename) = joinpath(test_files_dir(), filename)
 
 wav_file = test_file("test.wav")
@@ -17,32 +17,29 @@ frames_data = Frames(audiofile; winsize=256, winstep=128, type=hamming)
 @test length(frames_data) == 132
 
 audiofile = load(wav_file; sr=6000)
-frames    = Frames(audiofile)
+frames = Frames(audiofile)
 @test_nowarn get_data(frames)
-@test get_size(frames)    == 256
-@test get_step(frames)    == 128
+@test get_size(frames) == 256
+@test get_step(frames) == 128
 @test get_overlap(frames) == 128
 
 audiofile = load(wav_file; sr=10000)
-frames    = Frames(audiofile)
+frames = Frames(audiofile)
 @test_nowarn get_data(frames)
-@test get_size(frames)    == 512
-@test get_step(frames)    == 256
+@test get_size(frames) == 512
+@test get_step(frames) == 256
 @test get_overlap(frames) == 256
 
-audiofile = Audio911.load(mp3_file; mono=false)
-frames    = Frames(audiofile)
+audiofile = Audio911.load(mp3_file)
+frames = Frames(audiofile)
 @test_nowarn get_data(frames)
 
-# automatic mono conversion
-a1 = load(mp3_file; mono=false)
-a2 = load(mp3_file; mono=true)
-f1 = Frames(a1)
-f2 = Frames(a2)
-@test get_data(f1) == get_data(f2)
-
 # the `win` keyword accepts movingwindow(...) for compatibility
-f3 = Frames(a2; win=movingwindow(winsize=512, winstep=256))
-f4 = Frames(a2; winsize=512, winstep=256)
-@test get_data(f3) == get_data(f4)
-@test get_winsize(Stft(a2; win=movingwindow(winsize=1024))) == 1024
+a1 = load(mp3_file)
+f1 = Frames(a1; win=movingwindow(winsize=512, winstep=256))
+f1 = Frames(a1; winsize=512, winstep=256)
+@test get_data(f1) == get_data(f1)
+@test get_winsize(Stft(a1; win=movingwindow(winsize=1024))) == 1024
+
+@btime Frames(audiofile);
+# 3.493 μs (11 allocations: 10.41 KiB)
